ecb650a036026d480cb750a72d75d27a52f77a7f
Nayeli <Nayelioyuela913@gmail.com>
2023-11-23 06:38:58 -0600

    Prueba en main agregada

--- src/main/java/com/mycompany/herencia/Herencia.java
+++ src/main/java/com/mycompany/herencia/Herencia.java
@@ -11,6 +11,16 @@ package com.mycompany.herencia;
 public class Herencia {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+         // Ejemplo de uso
+        Persona persona = new Persona("Juan", 30, "Masculino");
+        persona.saludar();
+
+        Doctor doctor = new Doctor("Dra. Smith", 40, "Femenino", "Cardiología");
+        doctor.saludar();
+        doctor.tratarPaciente();
+
+        Deportista deportista = new Deportista("Carlos", 25, "Masculino", "Fútbol");
+        deportista.saludar();
+        deportista.entrenar();
     }
 }
